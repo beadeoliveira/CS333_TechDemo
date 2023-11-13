2ae5131af215233cfff0d481c68ba196b7789c93
beadeoliveira <beacdeoliveira@gmail.com>
2023-11-13 11:21:06 -0500

    - adding rankings calc and editing the close words

--- algos/hashing.py
+++ algos/hashing.py
@@ -6,27 +6,33 @@ from fuzzy_algo import fuzzy_calculation
 
 
 def hashing_moderation(input):
-    # first method is to take a set of unique words and parse through the words
+
     bad_words = pd.read_csv('../docs/bad_words.csv')
+    bad_words = bad_words.set_index('Words')
     processed_input = remove_punctuation(input)
     split_input = processed_input.split(' ')
     words = list(split_input)
     found_words = []
     leftover = []
-    print(words)
-    for w in range(len(words)):
+    w = 0
+    avg = 0
+
+    while w < len(words):
         word = words[w].lower()
-        # TODO: how to avoid making it pickup multiple in a sequence
         multi_check = multi_word(words, word, '', w)
-        if multi_check in bad_words.values:
+        if multi_check in bad_words.index:
             found_words.append(multi_check)
+            avg += bad_words.loc[multi_check]['ranking']
+            w += len(multi_check.split(' '))
         else:
             leftover.append(word)
+            w += 1
+
     if len(found_words) > 0:
-        return True, found_words
+        return True, found_words, avg/len(found_words), leftover
     return False, []
 
 
 if __name__ == "__main__":
-    input_val = "You are a piece of shit. Fuck you slut."
+    input_val = "You are a piece of shit. F*ck you slut."
     print(hashing_moderation(input_val))
